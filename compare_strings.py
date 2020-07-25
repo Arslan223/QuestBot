9612ff969cc04233567c73d3a95d389a55174fad
Ars <ars@Arss-MacBook-Air.local>
2020-07-25 14:30:23 +0300

    upd

--- compare_strings.py
+++ compare_strings.py
@@ -1,36 +1,31 @@
-
 import json
 import requests
 
 # 1. Get your authentication token
 # by posting username and password to the server
+def getSim(string1, string2, lang='ru', login='arslan2233', password='qwer2004'):
+    r = requests.post('http://paraphraser.ru/token/',
+                      data={'login': login, 'password': password})
+    token = r.json().get('token', '')
 
-r = requests.post('http://paraphraser.ru/token/',
-                  data={'login': 'user', 'password': '12345678'})
-token = r.json().get('token', '')
+    # 2. Make a request using the obtained token
 
-# 2. Make a request using the obtained token
+    payload = {'c': 'sim',
+               'query': string1+';'+string2,
+               'type': 'vector',
+               'format': 'json',
+               'lang': lang,
+               'token': token}
 
-payload = {'c': 'syns',
-           'query': 'кот ест рыбку',
-           'top': 3,
-           'scores': 0,
-           'forms': 0,
-           'format': 'json',
-           'lang': 'ru',
-           'token': token}
-
-r = requests.post('http://paraphraser.ru/api/',
-                  data=payload)
-result = r.json()
+    r = requests.post('http://paraphraser.ru/api/',
+                      data=payload)
+    result = r.json()
 
 
-if result['code'] == 0:
-    response = result['response']
+    if result['code'] == 0:
+        response = result['response']
 
-    for item in response:
-        for value in response[item]['syns']:
-            print(value)
+        return response['1']['sim']['score']
 
-else:
-    print('Error:', result['msg'])
+    else:
+        return 'Error:', result['msg']
